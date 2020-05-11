9c49c1e552ac1df841d91865828f43abba23fd6a
GardenLee <khjvvv7@gmail.com>
2020-05-11 22:32:26 +0900

    feat: add main method

---
+++ src/main/java/pitapat/party/dongdong/DongDong.java
@@ -0,0 +1,11 @@
+package pitapat.party.dongdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongDong {
+    public static void main(String[] args) {
+        SpringApplication.run(DongDong.class, args);
+    }
+}
